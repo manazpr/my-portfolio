003f8ee93759dd1a29bfe9f9b8d06e158f663e4b
Manaz PR <manas0096@gmail.com>
2022-07-27 00:45:12 +0530

    profile updated

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -15,16 +15,16 @@ const Who: FC = () => {
       />
       <div>
         <h1 data-scroll data-scroll-speed="0.5" className="title text-[40px]">
-          Hi there!
+          gm!
         </h1>
 
         {/* Hide my age :v */}
         <p data-scroll className="text-lg text-gray-200" id="story">
-          My name is Manaz P Ray. I&apos;m a frontend developer from India. I
-          have made a lot of projects, from basic HTML pages to complex projects
-          like Web3 Dapps with streaming payments. I hope to be a great
-          developer and get my dream job in the future. Besides coding, I also
-          like 3d Graphic Designing and Video Editing.
+          I am Manaz, a self-taught web developer/designer with interest in
+          NFTs, Gaming and Layer 2 Scaling solutions. I am deeply passionate
+          about working in Web3 and my key areas of interests include NFTs,
+          play-to-earn gaming and ZK Rollup tech. Currently I&apos;m learning
+          Three.js and Cairo.
         </p>
       </div>
     </div>
