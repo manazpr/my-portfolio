274ab23d656d6f54599fab69d833137f1db7b1c1
Manaz PR <manas0096@gmail.com>
2022-08-01 14:46:00 +0530

    fix: env variable

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,5 +1,5 @@
 import { GraphQLClient } from 'graphql-request';
 
 export const client = new GraphQLClient(
-  `https://api-ap-south-1.hygraph.com/v2/${process.env.GRAPHCMS_ENV}/master`
+  `https://api-ap-south-1.hygraph.com/v2/${process.env.NEXT_PUBLIC_GRAPHCMS}/master`
 );
